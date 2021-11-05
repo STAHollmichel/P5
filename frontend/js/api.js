840c677918201591724bf8e6339c1de47ba89f43
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-11-05 10:31:37 +0000

    HTML/JS: Correction on orthographic link to JS file and debugg on 1 camera fetching

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -14,3 +14,20 @@ async function getCameraListFromAPI() {
    );
    return cameraList;
 }
+
+async function getOneCameraFromAPI(id) { 
+  let camera = await fetch(`http://localhost:3000/api/cameras/${id}`)
+ .then( function (response) {
+   if(response.ok) {
+       return response.json();
+     } else {
+       console.log('error');
+     }
+   })
+   .then(function (camera){
+       console.log(camera);
+     return camera;  // Camera array call and their implementation on html
+   }
+ );
+ return camera;
+}
