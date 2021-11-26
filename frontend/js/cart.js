69561ba77fe870ebf082510acc6e233b7e613168
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-11-26 12:03:07 +0000

    Html/Js: Reformat and corrections of cart

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,7 +1,10 @@
 displayLocalStorage();
   
 function displayLocalStorage() {
-    const cartOrder = JSON.parse(localStorage.getItem("basket"))
+    const cartOrder = JSON.parse(localStorage.getItem("cart"))
+    console.log(cartOrder);
+    const priceList = cartOrder.map((item)=>item.price)
+    const total = priceList.reduce((a, b) => a + b)
     if(!cartOrder){
         document.querySelector("#cart_status").innerText = "Votre panier est vide !";
     }
@@ -10,28 +13,33 @@ function displayLocalStorage() {
             document.querySelector("#cart_status").innerText = "Votre panier contient :"
             document.querySelector("#cart").insertAdjacentHTML("beforeEnd", `
                 <div class="container-sm">
-                    <div class="cart-row">
-                        <span class="bg info col-sm-3" id="cart-item">ITEM</span>
-                        <span class="bg info col-sm-3" id=" price">PRIX</span>
-                        <span class="bg info col-sm-3" id="cart-quantity">QUANTITÉ</span>
-                    </div>
                     <div class="container-sm" id="cart-items">
                         <div class="container-sm" id="cart-item-row">
                             <div class="container-sm" id="cart-item cart-column">
                                 <img src="${item.imageUrl}" id="cart-img" alt="cart-item-image" width="100" height="100">
-                                <span class="bg info" id="cart-item-title">${item.name}</span><button class="btn btn-warning btn-remove" type="button">ENLEVER</button>
+                                <span class="bg info" id="cart-item-title"><strong>${item.name}</strong></span>
+                                <strong class="bg info" id ="cart-total-title">Prix</strong>
+                                <span class="bg info" id ="cart-item-price">${item.price/100},00 €</span>
                             </div>
+                        </div>
                     </div>
-                    <div class="bg info" id="cart-total">
-                    <strong class="bg info" id ="cart-total-title">Total</strong>
-                    <span class="info" id ="cart-total-price">,00 €</span>
-                    </div>
-
-                    <button class="btn btn-primary btn-purchase" type="button">Commander</button>
-                </div>
-                `
-            ); 
+                `);   
         }
+        document.querySelector("#cart_total").insertAdjacentHTML("beforeEnd", `
+                        <span class="bg info" id="total-cart-price"><strong>${total/100},00€</strong></span>`);
     }
 }
 
+
+async function clearCart() {
+    document.querySelector("#cart_clear").insertAdjacentHTML("beforeEnd", `
+        <button class="btn btn-warning" id="clearCart-btn" type="button">Vider le panier</button>`);
+    const BtnClearCart = document.querySelector("#clearCart-btn");
+        BtnClearCart.addEventListener("click", () => {
+        localStorage.clear(); //How to refresh action on Page?
+        window.alert("Panier vide !");
+        window.location.reload();
+    })         
+}
+
+clearCart();
