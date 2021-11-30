95557297eb283c707ba077b901b83e93b652e7d0
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-11-30 00:21:32 +0000

    JS: Correction on order Js

--- frontend/js/order.js
+++ frontend/js/order.js
@@ -3,7 +3,7 @@ async function orderRecap() {
 
 
     document.querySelector("#titleConfirm").innerHTML = `Félicitations ${Order.contact.firstName.toUpperCase()}, votre commande est validée !`;
-    document.querySelector("#orderLine").innerHTML = `Nous vous remercions d'avoir choisi Orinoco pour vos achats. Votre commande d'un montant total de ${Order.contact.totalCount} porte le numéro <strong>${Order.orderId}</strong>`;
+    document.querySelector("#orderLine").innerHTML = `Nous vous remercions d'avoir choisi Orinoco pour vos achats. Votre commande d'un montant total de ${Order.contact.total} porte le numéro <strong>${Order.orderId}</strong>`;
     document.querySelector("#delivery").innerHTML =`Elle vous sera envoyée dans les plus brefs délais à l'adresse que vous nous avez fournie lors de la validation à savoir : </br>
     ${Order.contact.address} ${Order.contact.postal} ${Order.contact.city} </br>
     Les informations de suivi vous serons envoyées par mail à l'adresse <i>${Order.contact.email}<i>`
