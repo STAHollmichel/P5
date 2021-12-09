524865792ebe9ae9878dd8395b4feba23f0bd76b
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-12-09 20:15:20 +0000

    HTML/CSS/JS: Cart Layout formating & Corrections

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -18,19 +18,23 @@ function displayLocalStorage() {
     else{
         for(let item of cartOrder){
             document.querySelector("#cart_status").innerText = "Votre panier contient :"
-            document.querySelector("#cart").insertAdjacentHTML("beforeEnd", `
-                <div class="container-sm">
-                    <div class="container-sm" id="cart-items">
-                        <div class="container-sm" id="cart-item-row">
-                            <div class="container-sm" id="cart-item cart-column">
-                                <img src="${item.imageUrl}" id="cart-img" alt="cart-item-image" width="100" height="100">
-                                <span class="bg info" id="cart-item-title"><strong>${item.name}</strong></span>
-                                <strong class="bg info" id ="cart-total-title">Prix</strong>
-                                <span class="bg info" id ="cart-item-price">${item.price/100},00 €</span>
+            document.querySelector("#cart").insertAdjacentHTML("beforeEnd", 
+                `
+                <div id="cart_card_container" class="col-lg-6 align-self-lg-center">
+                    <div class="card ori-bg0 mb-3" id="cart-items">
+                        <div class="d-flex justify-content-between">
+                            <picture id="cart-item-image">
+                                <img src="${item.imageUrl}" class="img-thumbnail rounded ori-bg0" id="cart-img" alt="cart-item-image">
+                            </picture>
+                            <div class="align-self-center" id="cart-item-title">
+                                <p class="fw-b">${item.name}</p>
+                            </div>
+                            <div class="align-self-center pe-2" id ="cart-item-price">
+                                <p class="fw-r">${item.price/100},00 €</p>
                             </div>
                         </div>
                     </div>
-                `);   
+                    `);   
         }
         document.querySelector("#cart_total").insertAdjacentHTML("beforeEnd", `
                         <span class="bg info" id="total-cart-price"><p class="fw-b">Total: ${total/100},00€<p></span>`);
