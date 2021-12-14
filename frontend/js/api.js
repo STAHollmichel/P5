271ac431cbe5b9c8516ff9aebe1ede579e2ca72f
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-12-14 06:16:01 +0000

    JS: Commenting & Indentation corrections to API.JS

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,37 +1,48 @@
-// Fetching of all cameras @ backend
+// Récupération de toutes les caméras dans le backend
 
 async function getCameraListFromAPI() { 
-    let cameraList = await fetch('http://localhost:3000/api/cameras')
-   .then( function (response) {
-     if(response.ok) {
-         return response.json();
-       } else {
-         console.log('error');
-       }
-     })
-     .then(function (cameraList){
-         console.log(cameraList);
-       return cameraList;  // Camera array call and their implementation on html
-     }
-   );
-   return cameraList;
+
+  let cameraList = await fetch('http://localhost:3000/api/cameras')
+
+    .then( function (response) {
+
+      if(response.ok) {
+        return response.json();
+
+      } else {
+        console.log('error');
+        }
+      })
+
+      .then(function (cameraList) {
+        console.log(cameraList);
+
+        return cameraList;  // Camera list array call and their implementation on html
+      }
+    );
+  return cameraList;
 }
 
-// Fetching of selected camera @ backend
+// Récupération d'une seule caméra dans le backend
 
 async function getOneCameraFromAPI(id) { 
+
   let camera = await fetch(`http://localhost:3000/api/cameras/${id}`)
- .then( function (response) {
-   if(response.ok) {
-       return response.json();
-     } else {
-       console.log('error');
-     }
-   })
-   .then(function (camera){
-       console.log(camera);
-     return camera;  // Camera call and their implementation on html
-   }
- );
- return camera;
+
+  .then( function (response) {
+
+    if(response.ok) {
+        return response.json();
+      } else {
+        console.log('error');
+      }
+    })
+
+    .then(function (camera) {
+      console.log(camera);
+
+      return camera;  // Camera call and their implementation on html
+    }
+  );
+  return camera;
 }
