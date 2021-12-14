63438fdbd41a065f80fbf77fb3328fe361a04991
Tiago de Andrea <sta.holllmichel@gmail.com>
2021-12-14 06:38:19 +0000

    JS: Commenting & Indentation corrections to main_page

--- frontend/js/main_page.js
+++ frontend/js/main_page.js
@@ -1,24 +1,31 @@
-// API fetching Response
+// Affichage des Cameras 
 
 function displayCameraList(cameraList) {
-  for (let camera of cameraList){
+
+  for (let camera of cameraList) {
+    
     document.querySelector("#camera-container").insertAdjacentHTML("beforeend", `
       <div class="camera card ori-bg0 m-3" data-id="${camera._id}">
-        <img src="${camera.imageUrl}" class="card-img-top" alt="camera home page display">
-        <div class="card-body text-center">
+          <picture>
+            <img src="${camera.imageUrl}" class="card-img-top" alt="camera home page display">
+          </picture>
+          <div class="card-body text-center">
           <h5 class="card-title">${camera.name}</h5>
-          <p class="card-text p-1">
           <p class="card-price p-1">Prix : ${camera.price /100},00 €</p>
           <a class="btn btn-outline-secondary" href="./product_page.html?id=${camera._id}">Voir Plus</a>
         </div>
       </div>`
-      ); 
-    }    
-  }
+    );
+  }    
+}
 
-  async function start() { 
-    let cameraList = await getCameraListFromAPI();
-    displayCameraList(cameraList);
-  }
+// Api Fetching
   
-  start();
+async function start() {
+
+  let cameraList = await getCameraListFromAPI();
+  
+  displayCameraList(cameraList);
+}
+
+start();
